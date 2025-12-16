71444cd1a5ce65e7c2f1bb72c615d7935fbe9fc0
Albert <albertoganesan5@gmail.com>
2025-12-16 17:45:40 +0700

    Добавил звук победы

--- Sokoban/Assets/Scripts/Sound/GameAudioController.cs
+++ Sokoban/Assets/Scripts/Sound/GameAudioController.cs
@@ -6,6 +6,25 @@ namespace Assets.Scripts.Sound
     {
         [SerializeField]
         private AudioClip WinSound;
+        [SerializeField]
+        private AudioSource audioSource;
+        [SerializeField]
+        private ScoreManager scoreManager;
 
+        private void Start()
+        {
+            if (audioSource == null)
+                return;
+            audioSource.volume = PlayerPrefs.GetFloat("MasterVolume", 1f);
+
+            if (scoreManager == null)
+                return;
+            scoreManager.OnWin += PlayWinSound;
+        }
+
+        private void PlayWinSound()
+        {
+            audioSource.PlayOneShot(WinSound);
+        }
     }
 }
